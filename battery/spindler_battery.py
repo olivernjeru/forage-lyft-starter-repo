818adbdd6ff10b99f8663a02c4fe9a8fb2d5d239
Oliver Njeru <olivernjiru@gmail.com>
2023-12-26 23:40:35 +0300

    fix minor bug

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -2,7 +2,7 @@ from battery.battery import Battery
 from utils import add_years_to_date
 
 
-class SpundlerBattery(Battery):
+class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
