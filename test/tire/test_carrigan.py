c0449183cc8fbaee2ad6dce6f944c26b9a03296d
Oliver Njeru <olivernjiru@gmail.com>
2023-12-27 17:39:45 +0300

    refactor tire component

--- test/tire/test_carrigan.py
+++ test/tire/test_carrigan.py
@@ -1,19 +1,19 @@
 import unittest
 
 
-from tire.carrigan import CarriganTire
+from tire.carrigan_tire import CarriganTire
 
 
 
 class TestCarrigan(unittest.TestCase):
     def test_tire_should_be_serviced(self):
-        sensor_data = [0.2, 0.5, 0.8, 0.3]
+        sensor_data = [0.2, 0.5, 0.9, 0.3]
 
         tire = CarriganTire(sensor_data)
         self.assertTrue(tire.needs_service())
 
     def test_tire_should_not_be_serviced(self):
-        sensor_data = [0.2, 0.5, 0.9, 0.3]
+        sensor_data = [0.2, 0.5, 0.8, 0.3]
 
         tire = CarriganTire(sensor_data)
         self.assertFalse(tire.needs_service())
--- test/tire/test_octoprime.py
+++ test/tire/test_octoprime.py
@@ -1,19 +1,19 @@
 import unittest
 
 
-from tire.octoprime import OctoprimeTire
+from tire.octoprime_tire import OctoprimeTire
 
 
 
 class TestOctoprime(unittest.TestCase):
     def test_tire_should_be_serviced(self):
-        sensor_data = [0.2, 0.5, 0.8, 0.3]
+        sensor_data = [0.9, 0.8, 0.9, 0.6]
 
         tire = OctoprimeTire(sensor_data)
         self.assertTrue(tire.needs_service())
 
     def test_tire_should_not_be_serviced(self):
-        sensor_data = [0.9, 0.8, 0.9, 0.6]
+        sensor_data = [0.2, 0.5, 0.8, 0.3]
 
         tire = OctoprimeTire(sensor_data)
         self.assertFalse(tire.needs_service())
